5e1b08bab37d4f48b992a552b6e4bd990cd7ec85
siimoad <38928626+siimoad@users.noreply.github.com>
2019-07-08 16:55:30 +0000

    First Commit

---
+++ booking/resources/views/layouts/app.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'IT-Genious') }}</title>
+        <!-- Favicon -->
+        <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+        <!-- Icons -->
+        <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+        <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+        <!-- Argon CSS -->
+        <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
+        <!-- Gretong CSS -->
+                <link type="text/css" href="{{ asset('gretong') }}/css/style.css" rel="stylesheet">
+        <link type="text/css" href="{{ asset('gretong') }}/css/bootstrap.css" rel="stylesheet">
+        <link type="text/css" href="{{ asset('gretong') }}/css/etalage.css" rel="stylesheet">
+        <link type="text/css" href="{{ asset('gretong') }}/css/megamenu.css" rel="stylesheet">
+
+    </head>
+    <body class="{{ $class ?? '' }}">
+        @auth()
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            @include('layouts.navbars.sidebar')
+        @endauth
+        
+        <div class="main-content">
+            @include('layouts.navbars.navbar')
+            @yield('content')
+        </div>
+
+        @guest()
+            @include('layouts.footers.guest')
+        @endguest
+
+        <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
+        <script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        
+        @stack('js')
+        
+        <!-- Argon JS -->
+        <script src="{{ asset('argon') }}/js/argon.js?v=1.0.0"></script>
+        <!-- Gretong JS -->
+        <script src="{{ asset('gretong') }}/js/jquery-1.11.1.min.js"></script>
+        <script src="{{ asset('gretong') }}/js/jquery-ui.min.js"></script>
+        <script src="{{ asset('gretong') }}/js/jquery.etalage.min.js"></script>
+        <script src="{{ asset('gretong') }}/js/jquery.jscrollpane.min.js"></script>
+        <script src="{{ asset('gretong') }}/js/megamenu.js"></script>
+        <script src="{{ asset('gretong') }}/js/menu_jqery.js"></script>
+        <script src="{{ asset('gretong') }}/js/simpleChart.min.js"></script>
+    </body>
+</html>
