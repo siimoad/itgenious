e2e86c194244e3bb043f0782118461d93f2f3627
hicham <sbihi.hicham96@gmail.com>
2019-07-09 18:06:26 +0000

    Rebuild app.blade.php

--- booking/resources/views/layouts/navbars/navs/guest.blade.php
+++ booking/resources/views/layouts/navbars/navs/guest.blade.php
@@ -9,29 +9,12 @@
 				<div class="rgt-bottom">
 					<div class="log">
 						<div class="login" >
-							<div id="loginContainer"><a href="{{ route('login') }}" id="loginButton"><span>Login</span></a>
-								<div id="loginBox">                
-									<form id="loginForm">
-											<fieldset id="body">
-												<fieldset>
-													<label for="email">Email Address</label>
-													<input type="text" name="email" id="email">
-												</fieldset>
-												<fieldset>
-														<label for="password">Password</label>
-														<input type="password" name="password" id="password">
-												</fieldset>
-												<input type="submit" id="login" value="Sign in">
-												<label for="checkbox"><input type="checkbox" id="checkbox"> <i>Remember me</i></label>
-											</fieldset>
-										<span><a href="{{ route('password.request') }}">Forgot your password?</a></span>
-									</form>
-								</div>
+							<div id="loginContainer"><a href="#" id="loginButton"><span>Login</span></a>
 							</div>
 						</div>
 					</div>
 					<div class="reg">
-						<a href="{{ route('register') }}">REGISTER</a>
+						<a href="register.html">REGISTER</a>
 					</div>
 				<div class="cart box_1">
 					<a href="checkout.html">
@@ -50,6 +33,50 @@
 			<div class="clearfix"> </div>
 		</div>
 		<!-- start header menu -->
+		<ul class="megamenu skyblue">
+			<li class="active grid"><a class="color1" href="index.html">Home</a></li>
+			<li class="grid"><a class="color2" href="#">new arrivals</a></li>
+			<li><a class="color4" href="#">TUXEDO</a></li>
+			<li><a class="color5" href="#">SWEATER</a></li>
+			<li><a class="color6" href="#">SHOES</a></li>			
+			<li><a class="color7" href="#">GLASSES</a></li>
+			<li><a class="color8" href="#">T-SHIRT</a></li>
+			<li><a class="color9" href="#">WATCHES</a></li>
+		 </ul> 
 	</div>
 </div>
-
+<div class="arriv">
+	<div class="container">
+		<div class="arriv-top">	
+			<div class="col-md-6 arriv-left">
+				<a href="details.html"><img src="{{ asset('gretong') }}/images/Microsoft-Certification-Logo.png" class="img-responsive" alt=""></a>
+			</div>
+			<div class="col-md-6 arriv-right">
+				<a href="details.html"><img src="{{ asset('gretong') }}/images/htmljscss.png" class="img-responsive" alt=""></a>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<div class="arriv-bottm">
+			<div class="col-md-8 arriv-left1">
+				<a href="details.html"><img src="{{ asset('gretong') }}/images/oracle.png" class="img-responsive" alt=""></a>
+			</div>
+			<div class="col-md-4 arriv-right1">
+				<a href="details.html"><img src="{{ asset('gretong') }}/images/linux.png" class="img-responsive" alt=""></a>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<div class="arriv-las">
+			<div class="col-md-4 arriv-left2">
+				<a href="details.html"><img src="{{ asset('gretong') }}/images/fortinet-inner.png" class="img-responsive" alt=""></a>
+			</div>
+			<div class="col-md-4 arriv-middle">
+				<a href="details.html"><img src="{{ asset('gretong') }}/images/java.png" class="img-responsive" alt=""></a>
+			</div>
+			<div class="col-md-4 arriv-right2">
+				<a href="details.html"><img src="{{ asset('gretong') }}/images/cisco.png" class="img-responsive" alt=""></a>
+				
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
